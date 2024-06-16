f6ccc4ca16cae1944d7e36d5ebcb21b29796bf12
kumamoto <ben@leblond.us>
2024-06-16 15:22:38 -0500

    initial homebrew formula

---
+++ celest.rb
@@ -0,0 +1,47 @@
+class Celest < Formula
+  desc "The flutter cloud platform"
+  homepage "https://celest.dev"
+
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://releases.celest.dev/macos_arm64/latest/celest-latest-macos_arm64.pkg"
+      sha256 "e70b351443e782d8ecc49e7a36eb6efee3ee5a4557e6040f956ae2625e222427"
+      filename = "celest-latest-macos_arm64.pkg"
+    else
+      url "https://releases.celest.dev/macos_x64/latest/celest-latest-macos_x64.pkg"
+      sha256 "725af8d398f18d50f913805907fa2d4b7a8c81f8697f28da7a8f7c2d115f2594"
+      filename = "celest-latest-macos_x64.pkg"
+    end
+  elsif OS.linux?
+    if Hardware::CPU.arm?
+      url "https://releases.celest.dev/linux_arm64/latest/celest-latest-linux_arm64.deb"
+      sha256 "eb459565228cf82552a06a58ba69371261db233ae48c576ae7d430c7da12c7d6"
+      filename = "celest-latest-linux_arm64.deb"
+    else
+      url "https://releases.celest.dev/linux_x64/latest/celest-latest-linux_x64.deb"
+      sha256 "e9b5b80d493ab61424ffcb6427092ecc7d2e09ed95548900bf53ccd4471ba899"
+      filename = "celest-latest-linux_x64.deb"
+    end
+  end
+
+  license "MIT"
+
+  def install
+    if OS.mac?
+      system "installer", "-pkg", filename, "-target", "/"
+    elsif OS.linux?
+      system "dpkg", "-i", filename
+    end
+  end
+
+  def caveats
+    <<~EOS
+      For macOS, the package has been installed using the macOS installer.
+      For Linux, the package has been installed using dpkg.
+    EOS
+  end
+
+  test do
+    system "#{bin}/celest", "--version"
+  end
+end
---
+++ update_sha256.dart
@@ -0,0 +1,118 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:consola/consola.dart';
+import 'package:crypto/crypto.dart';
+import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as path;
+
+Future<void> main() async {
+  Console.clearScreen();
+
+  final architectures = {
+    'macos_arm64':
+        'https://releases.celest.dev/macos_arm64/latest/celest-latest-macos_arm64.pkg',
+    'macos_x64':
+        'https://releases.celest.dev/macos_x64/latest/celest-latest-macos_x64.pkg',
+    'linux_arm64':
+        'https://releases.celest.dev/linux_arm64/latest/celest-latest-linux_arm64.deb',
+    'linux_x64':
+        'https://releases.celest.dev/linux_x64/latest/celest-latest-linux_x64.deb'
+  };
+
+  final sha256s = <String, String>{};
+
+  for (final arch in architectures.entries) {
+    final fileName = arch.key.split('_').join('-');
+    final filePath = await downloadFile(arch.value, fileName);
+    final sha256 = await calculateSha256(filePath);
+    sha256s[arch.key] = sha256;
+  }
+
+  await updateFormula(sha256s);
+  Console.writeLine('Homebrew formula updated with new SHA-256 checksums.');
+}
+
+Future<String> downloadFile(String url, String fileName) async {
+  String tempPath = Directory.systemTemp.path;
+
+  final file = File(path.join(tempPath, fileName));
+
+  // check if the file already exists, if so skip the download
+  if (file.existsSync()) {
+    Console.writeLine('File $fileName already exists, skipping download.');
+    return file.path;
+  }
+
+  final client = http.Client();
+  final request = await client.send(http.Request('GET', Uri.parse(url)));
+
+  int totalBytes = 0;
+  final stopwatch = Stopwatch()..start();
+
+  final sink = file.openWrite();
+  final contentLength = int.parse(request.headers['content-length']!);
+  final progressBar = ProgressBar.atCursor(
+    total: contentLength,
+    width: 100,
+    head: 'Downloading $fileName: ',
+    barFillCharacter: '#',
+    tailBuilder: (_, __, percent) => ' ${percent.toStringAsFixed(0)}%',
+    console: Console.instance,
+  );
+
+  final completer = Completer<void>();
+
+  request.stream.listen((chunk) {
+    totalBytes += chunk.length;
+    sink.add(chunk);
+
+    progressBar.current = totalBytes;
+    Console.draw(progressBar);
+  }, onDone: () async {
+    stopwatch.stop();
+    await sink.close();
+    completer.complete();
+  }, onError: (error) {
+    completer.completeError(error);
+  });
+
+  await completer.future;
+  return file.path;
+}
+
+Future<String> calculateSha256(String filePath) async {
+  final file = File(filePath);
+  final bytes = await file.readAsBytes();
+  final digest = sha256.convert(bytes);
+  return digest.toString();
+}
+
+Future<void> updateFormula(Map<String, String> sha256s) async {
+  final formulaTemplatePath = 'celest.rb.template';
+  final formulaPath = 'celest.rb';
+  final formulaTemplateFile = File(formulaTemplatePath);
+  final formulaFile = File(formulaPath);
+  final formulaTemplate = await formulaTemplateFile.readAsString();
+
+  // null check the sha256s
+  if (sha256s['macos_arm64'] == null ||
+      sha256s['macos_x64'] == null ||
+      sha256s['linux_arm64'] == null ||
+      sha256s['linux_x64'] == null) {
+    throw Exception('SHA-256 checksums are null.');
+  }
+
+  final updatedFormula = formulaTemplate
+      .replaceFirst(RegExp(r'SHA256_CHECKSUM_OF_ARM64_MACOS_PKG'),
+          sha256s['macos_arm64']!)
+      .replaceFirst(
+          RegExp(r'SHA256_CHECKSUM_OF_X86_64_MACOS_PKG'), sha256s['macos_x64']!)
+      .replaceFirst(RegExp(r'SHA256_CHECKSUM_OF_ARM64_LINUX_DEB'),
+          sha256s['linux_arm64']!)
+      .replaceFirst(RegExp(r'SHA256_CHECKSUM_OF_X86_64_LINUX_DEB'),
+          sha256s['linux_x64']!);
+
+  // write the formula to a file named celest.rb
+  await formulaFile.writeAsString(updatedFormula);
+}
